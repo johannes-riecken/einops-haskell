7f91c103ae01eb7e7698f520f7e7e8cc309223d8
Johannes Riecken <johannes.riecken@gmail.com>
2022-05-15 20:29:51 +0200

    Add inputCompositeAxes

--- Main.hs
+++ Main.hs
@@ -1,16 +1,21 @@
+{-# LANGUAGE LambdaCase #-}
 import Data.Maybe
+import qualified Data.Set as S
+import Data.Set (Set(..))
+import Data.List
+import Control.Arrow
 
 data Axis = I | J | Ellipsis deriving (Eq, Ord, Show, Bounded, Enum)
 
-data Composite = Single Axis | Multiple [Axis] deriving (Show)
+data Composite a = Single a | Multiple [a] deriving (Show)
 
-flatten :: [Composite] -> [Axis]
+flatten :: [Composite a] -> [a]
 flatten (Single x : xs) = x : flatten xs
 flatten (Multiple xs : xs') = xs ++ flatten xs'
 
-data Equation = Equation {
-    input :: [Composite],
-    output :: [Composite]
+data Equation a = Equation {
+    input :: [Composite a],
+    output :: [Composite a]
     } deriving (Show)
 
 -- axesPermutation gives the numbers of flatten output axes
@@ -23,11 +28,21 @@ axesPermutation = map fromEnum
 ellipsisPositionInLhs :: [Axis] -> Maybe Int
 ellipsisPositionInLhs = fmap fst . listToMaybe . filter (\(_,a) -> a == Ellipsis) . zip [0..]
 
--- inputCompositeAxes known xs = 
+-- inputCompositeAxes returns a list that for each composite axis returns its
+-- tuple of known and unknown axis numbers
+inputCompositeAxes :: Set Axis -> [Composite Axis] -> [([Int],[Int])]
+inputCompositeAxes known xs = map (map fromEnum *** map fromEnum) xs' where
+        xs' = map (\case
+            Single x -> case x `S.member` known of
+                True -> ([x], [])
+                False -> ([], [x])
+            Multiple xs -> partition (`S.member` known) xs
+            ) xs
 
 main :: IO ()
 main = do
     print $ axesPermutation [J, I]
     print $ ellipsisPositionInLhs [I, Ellipsis, J]
     print $ ellipsisPositionInLhs [I, J]
+    print $ inputCompositeAxes S.empty [Single I, Single J]
     pure ()
