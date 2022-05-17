45214efa600e7b75175e7e53c50d7636c53bc456
Johannes Riecken <johannes.riecken@gmail.com>
2022-05-17 21:35:19 +0200

    Refactor for polytypic programming

--- Main.hs
+++ Main.hs
@@ -1,17 +1,17 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DeriveTraversable, LambdaCase #-}
 import Data.Maybe
 import qualified Data.Set as S
 import Data.Set (Set(..))
 import Data.List
 import Control.Arrow
+import qualified Data.Foldable as F
 
 data Axis = I | J | Ellipsis deriving (Eq, Ord, Show, Bounded, Enum)
 
-data Composite a = Single a | Multiple [a] deriving (Show)
+data Composite a = Single a | Multiple [a] deriving (Functor, Foldable, Traversable, Show)
 
 flatten :: [Composite a] -> [a]
-flatten (Single x : xs) = x : flatten xs
-flatten (Multiple xs : xs') = xs ++ flatten xs'
+flatten = (=<<) F.toList
 
 data Equation a = Equation {
     input :: [Composite a],
@@ -30,19 +30,20 @@ ellipsisPositionInLhs = fmap fst . listToMaybe . filter (\(_,a) -> a == Ellipsis
 
 -- inputCompositeAxes returns a list that for each composite axis returns its
 -- tuple of known and unknown axis numbers
-inputCompositeAxes :: Set Axis -> [Composite Axis] -> [([Int],[Int])]
-inputCompositeAxes known xs = map (map fromEnum *** map fromEnum) xs' where
-        xs' = map (\case
-            Single x -> case x `S.member` known of
-                True -> ([x], [])
-                False -> ([], [x])
-            Multiple xs -> partition (`S.member` known) xs
+inputCompositeAxes :: Set Int -> [Composite Int] -> [([Int],[Int])]
+inputCompositeAxes known xs = map (
+            partition ((`S.member` known)) . F.toList
             ) xs
+
+
+toLists :: [Composite a] -> [[a]]
+toLists = map F.toList
 
 main :: IO ()
 main = do
     print $ axesPermutation [J, I]
     print $ ellipsisPositionInLhs [I, Ellipsis, J]
     print $ ellipsisPositionInLhs [I, J]
-    print $ inputCompositeAxes S.empty [Single I, Single J]
+    print $ inputCompositeAxes S.empty $ fmap (fmap fromEnum) [Single I, Single J]
+
     pure ()
