60c26d073a3345a1369a64d15ae8984835570d32
Johannes Riecken <johannes.riecken@gmail.com>
2022-05-19 20:40:16 +0200

    Make Python intermediate results quick-checkable

--- Main.hs
+++ Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveTraversable, LambdaCase #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric, DataKinds, TypeOperators #-}
 import Data.Maybe
 import qualified Data.Set as S
 import Data.Set (Set(..))
@@ -6,9 +7,63 @@ import Data.List
 import Control.Arrow
 import qualified Data.Foldable as F
 
-data Axis = I | J | Ellipsis deriving (Eq, Ord, Show, Bounded, Enum)
+import Data.Either (fromRight)
+import Servant.API
+import Servant.Client
+import Servant.Client.Core.Request hiding (RequestBodyLBS, requestHeaders, requestBody)
+import GHC.Generics
+import Data.Proxy
+import Data.Aeson hiding ((.:))
+import Network.HTTP.Client hiding (Proxy(..), cookieJar)
+import Network.HTTP.Simple hiding (Proxy(..))
+import qualified Data.Sequence as S
+import Data.Function.Pointless
+import Data.Either.Extra (eitherToMaybe)
+import Test.QuickCheck
+import System.IO.Unsafe
+import Safe
+import qualified Data.ByteString.Lazy.Char8 as BS
 
-data Composite a = Single a | Multiple [a] deriving (Functor, Foldable, Traversable, Show)
+data Axis = I | J | Ellipsis deriving (Eq, Ord, Bounded, Enum, Generic)
+
+instance Show Axis where
+    show I = "i"
+    show J = "j"
+    show Ellipsis = "..."
+
+instance Arbitrary Axis where
+    arbitrary = elements [I, J, Ellipsis]
+
+instance ToJSON Axis
+
+data Composite a = Single a | Multiple [a] deriving (Functor, Foldable, Traversable, Show, Generic)
+
+instance Arbitrary a => Arbitrary (Composite a) where
+    arbitrary = oneof [Single <$> arbitrary, Multiple <$> arbitrary]
+
+instance ToJSON a => ToJSON (Composite a)
+
+-- type EinOpsAPI = "/" :> ReqBody '[JSON] (Equation Axis) :> Post '[JSON] [Int]
+type EinOpsAPI = "/" :> ReqBody '[JSON] EquationStr :> Post '[JSON] [Int]
+
+newtype EquationStr = EquationStr { eqn :: String } deriving (Generic, Show)
+
+instance ToJSON EquationStr
+
+einOpsAPI :: Proxy EinOpsAPI
+einOpsAPI = Proxy
+
+einOpsRequest :: EquationStr -> ClientM [Int]
+einOpsRequest = client einOpsAPI
+
+einOps :: Equation Axis -> [Int]
+-- einOps :: Equation Axis -> Either ClientError [Int]
+einOps xs = (fromRight [777]) . unsafePerformIO $ do
+-- einOps xs = unsafePerformIO $ do
+    mngr <- newManager defaultManagerSettings
+    res <- runClientM (einOpsRequest . EquationStr . eqnToStr $ xs) (
+        mkClientEnv mngr (BaseUrl Http "127.0.0.1" 5000 ""))
+    return res
 
 flatten :: [Composite a] -> [a]
 flatten = (=<<) F.toList
@@ -16,7 +71,22 @@ flatten = (=<<) F.toList
 data Equation a = Equation {
     input :: [Composite a],
     output :: [Composite a]
-    } deriving (Show)
+    } deriving (Show, Generic)
+
+instance Arbitrary a => Arbitrary (Equation a) where
+    arbitrary = Equation <$> arbitrary <*> arbitrary
+
+instance ToJSON a => ToJSON (Equation a)
+
+eqnToStr :: Show a => Equation a -> String
+eqnToStr (Equation i o) = compsToStr i <> " -> " <> compsToStr o
+
+compsToStr :: Show a => [Composite a] -> String
+compsToStr = intercalate " " . fmap compToStr
+
+compToStr :: Show a => Composite a -> String
+compToStr (Single x) = show x
+compToStr (Multiple xs) = "(" <> intercalate " " (fmap show xs) <> ")"
 
 -- axesPermutation gives the numbers of flatten output axes
 axesPermutation :: [Axis] -> [Int]
@@ -39,11 +109,24 @@ inputCompositeAxes known xs = map (
 toLists :: [Composite a] -> [[a]]
 toLists = map F.toList
 
+emptyAxis :: [Composite Axis]
+emptyAxis = []
+
+iAxis :: [Composite Axis]
+iAxis = [Single I]
+
+ijeAxis = [Multiple [I,J], Single Ellipsis]
+
 main :: IO ()
 main = do
-    print $ axesPermutation [J, I]
-    print $ ellipsisPositionInLhs [I, Ellipsis, J]
-    print $ ellipsisPositionInLhs [I, J]
-    print $ inputCompositeAxes S.empty $ fmap (fmap fromEnum) [Single I, Single J]
+    -- BS.putStrLn $ encode (EquationStr " -> ")
+    -- putStrLn . eqnToStr $ Equation iAxis iAxis
+    -- putStrLn . eqnToStr $ Equation iAxis ijeAxis
+    -- print $ einOps (Equation iAxis iAxis)
+    quickCheck $ \xs -> einOps xs === axesPermutation (flatten . input $ xs)
+    -- print $ axesPermutation [J, I]
+    -- print $ ellipsisPositionInLhs [I, Ellipsis, J]
+    -- print $ ellipsisPositionInLhs [I, J]
+    -- print $ inputCompositeAxes S.empty $ fmap (fmap fromEnum) [Single I, Single J]
 
     pure ()
